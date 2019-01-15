9b000023e43688b4a94ae223cfa1d76d563734d7
Blumen Herzenschein <30454577+herzenschein@users.noreply.github.com>
2019-01-15 16:19:41 -0200

    Add script to move files to proper places

---
+++ adjust.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir $HOME/bin
+export PATH=$PATH:$HOME/bin
+mv bin/ls-konqi $HOME/bin/
+mv notify-konqi $HOME/bin/
+mv .bash_aliases $HOME/.bash_aliases
+mv kdeconnect-cli.info $HOME/Desktop
