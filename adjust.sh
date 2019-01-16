b86dd1282e8d48a5c7d4021cfccd9f053e2e78b5
Blumen Herzenschein <30454577+herzenschein@users.noreply.github.com>
2019-01-15 23:31:15 -0200

    Fix info path

--- adjust.sh
+++ adjust.sh
@@ -4,4 +4,4 @@ echo 'export PATH="$HOME/bin:$PATH"' >> $HOME/.bashrc
 mv bin/ls-konqi $HOME/bin/
 mv bin/notify-konqi $HOME/bin/
 mv .bash_aliases $HOME/.bash_aliases
-mv kdeconnect-cli.info $HOME/Desktop
+mv kdeconnect-cli.info $HOME/Desktop/
