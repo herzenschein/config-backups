a462fab06d37c25bf41d8bfa91f1919cef7580ca
Blumen Herzenschein <30454577+herzenschein@users.noreply.github.com>
2019-01-15 22:55:40 -0200

    Fix notify-konqi path

--- adjust.sh
+++ adjust.sh
@@ -2,6 +2,6 @@
 mkdir $HOME/bin
 export PATH=$PATH:$HOME/bin
 mv bin/ls-konqi $HOME/bin/
-mv notify-konqi $HOME/bin/
+mv bin/notify-konqi $HOME/bin/
 mv .bash_aliases $HOME/.bash_aliases
 mv kdeconnect-cli.info $HOME/Desktop
