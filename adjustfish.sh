d700be948f75cfa82a57406c9304e7fd0d8c5dbb
Blumen Herzenschein <30454577+herzenschein@users.noreply.github.com>
2019-01-15 23:31:50 -0200

    Fix info path and filename

--- adjustfish.sh
+++ adjustfish.sh
@@ -4,4 +4,4 @@ echo 'export PATH="$HOME/bin:$PATH"' >> $HOME/.bashrc
 mv bin/ls-konqi $HOME/bin/
 mv bin/notify-konqi $HOME/bin/
 mv config.fish $HOME/.config/fish/
-mv kdeconnect-cli.info $HOME/Desktop
+mv kdeconnect-cli.info $HOME/Desktop/
