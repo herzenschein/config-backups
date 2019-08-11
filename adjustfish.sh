5e2f69157986fdbeeb96393027a31a7836b356ed
Thiago Sueto <30454577+herzenschein@users.noreply.github.com>
2019-08-10 21:47:07 -0300

    Delete adjustfish.sh

--- adjustfish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir $HOME/bin
-echo 'export PATH="$HOME/bin:$PATH"' >> $HOME/.bashrc
-mv bin/ls-konqi $HOME/bin/
-mv bin/notify-konqi $HOME/bin/
-mv config.fish $HOME/.config/fish/
-mv kdeconnect-cli.info $HOME/Desktop/
-mv kglobalshortcutsrc $HOME/.config/
